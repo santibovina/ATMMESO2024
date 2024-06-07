9cc1e44444c595ffe66ba01edfae037bd609ff7a
Santiago Bovina <110306838+santibovina@users.noreply.github.com>
2024-06-07 11:21:31 -0300

    agregar modelos

--- cajeros/operaciones/admin.py
+++ cajeros/operaciones/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Cajero, Gaveta, Operacion, Banco
+from .models import Cajero, Gaveta, Operacion, Banco, Billete, Modelo, Tipologia, TipoDiferencia, Portavalor, Diferencias
 
 admin.site.register(Banco)
 admin.site.register(Cajero)
 admin.site.register(Gaveta)
 admin.site.register(Operacion)
+admin.site.register(Billete)
+admin.site.register(Modelo)
+admin.site.register(Tipologia)
+admin.site.register(TipoDiferencia)
+admin.site.register(Portavalor)
+admin.site.register(Diferencias)
--- cajeros/operaciones/models.py
+++ cajeros/operaciones/models.py
@@ -14,24 +14,70 @@ class Cajero(models.Model):
 
     def __str__(self):
         return f"{self.id_cajero} - {self.banco} - {self.ubicacion}"
+    
+class Billete(models.Model):
+    billete_denominacion = models.IntegerField()
+
+    def __str__(self):
+        return f"${self.billete_denominacion}"
+
+class Modelo(models.Model):
+    modelo_gaveta = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.modelo_gaveta
+    
+class Tipologia(models.Model):
+    tipologia_gaveta = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.tipologia_gaveta
 
 class Gaveta(models.Model):
     id_gaveta = models.CharField(max_length=100, unique=True)
-    denominacion_billete = models.DecimalField(max_digits=5, decimal_places=0)
     banco = models.ForeignKey(Banco, on_delete=models.CASCADE)
+    denominacion_billete = models.ForeignKey(Billete, on_delete=models.CASCADE)
+    modelo = models.ForeignKey(Modelo, on_delete=models.CASCADE, null=True, blank=True)
+    tipologia = models.ForeignKey(Tipologia, on_delete=models.CASCADE, null=True, blank=True)
     cajero = models.ForeignKey(Cajero, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.id_gaveta} - ${self.denominacion_billete}"
+        return f"{self.id_gaveta} - {self.modelo} - {self.tipologia} - {self.denominacion_billete}"
 
 class Operacion(models.Model):
     fecha_operacion = models.DateTimeField(auto_now_add=True)
     fecha_actualizacion = models.DateField()
     cajero = models.ForeignKey(Cajero, on_delete=models.CASCADE)
     gaveta = models.ForeignKey(Gaveta, on_delete=models.CASCADE)
-    numero_precinto = models.CharField(max_length=100)
-    total_por_denominacion = models.DecimalField(max_digits=10, decimal_places=0)
+    numero_precinto = models.CharField(max_length=100, unique=True)
+    billete = models.ForeignKey(Billete, on_delete=models.CASCADE, null=True, blank=True)
+    total_por_denominacion = models.IntegerField()
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.fecha_operacion.strftime("%d/%m/%Y")} - {self.cajero} - ${self.gaveta.denominacion_billete} - ${self.total_por_denominacion} - {self.gaveta.id_gaveta} - {self.usuario}"
+    
+class TipoDiferencia(models.Model):
+    tipo_diferencia = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.tipo_diferencia
+    
+class Portavalor(models.Model):
+    id_portavalor = models.CharField(max_length=10, unique=True)
+    nombre_portavalor = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.id_portavalor} - {self.nombre_portavalor}"
+    
+class Diferencias(models.Model):
+    cajero = models.ForeignKey(Cajero, on_delete=models.CASCADE)
+    fecha_actualizacion = models.DateField()
+    tipo_diferencia = models.ForeignKey(TipoDiferencia, on_delete=models.CASCADE)
+    recorrido = models.IntegerField()
+    portavalor = models.ForeignKey(Portavalor, on_delete=models.CASCADE)
+    total_diferencia = models.IntegerField()
+    observaciones = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.fecha} - {self.cajero} - {self.tipo_diferencia} - {self.total_diferencia} - {self.observaciones} - {self.portavalor}"
