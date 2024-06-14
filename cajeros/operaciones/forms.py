301b60d6890a6c30530381ddff248fd07607db38
Santiago Bovina <110306838+santibovina@users.noreply.github.com>
2024-06-14 11:17:36 -0300

    Cambiar forms.py

--- cajeros/operaciones/forms.py
+++ cajeros/operaciones/forms.py
@@ -5,14 +5,10 @@ from django.forms.models import inlineformset_factory
 class OperacionForm(forms.ModelForm):
     class Meta:
         model = Operacion
-        fields = ['fecha_habilitacion', 'cajero', 'precinto_gaveta', 'gaveta', 'total_por_gaveta',]
-        
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     gaveta = cleaned_data.get("gaveta")
-    #     total_por_denominacion = cleaned_data.get("total_por_denominacion")
-
-    #     if gaveta and total_por_denominacion:
-    #         if gaveta.denominacion_billete != total_por_denominacion:
-    #             raise forms.ValidationError("La denominación del billete no coincide con la de la gaveta.")
-    #     return cleaned_data
+        fields = ['fecha_habilitacion',
+                  'cajero',
+                  'gaveta',
+                  'precinto_gaveta',
+                  'total_por_gaveta',
+                  'precinto_depurador',
+                  'precinto_bolso']
